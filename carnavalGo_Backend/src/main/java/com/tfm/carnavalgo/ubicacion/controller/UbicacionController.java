d58b433ed7b42f95d5f8ac0784719404754f93b7
lrodriguezol <lrodriguezol@uoc.edu>
2025-05-12 19:43:56 +0200

    eliminacion import

--- carnavalGo_Backend/src/main/java/com/tfm/carnavalgo/ubicacion/controller/UbicacionController.java
+++ carnavalGo_Backend/src/main/java/com/tfm/carnavalgo/ubicacion/controller/UbicacionController.java
@@ -1,11 +1,9 @@
 package com.tfm.carnavalgo.ubicacion.controller;
 
-import com.tfm.carnavalgo.agrupacion.model.Agrupacion;
-import com.tfm.carnavalgo.agrupacion.repository.AgrupacionRepository;
 import com.tfm.carnavalgo.ubicacion.model.Ubicacion;
 import com.tfm.carnavalgo.ubicacion.service.UbicacionService;
 import com.tfm.carnavalgo.usuario.model.Usuario;
-import com.tfm.carnavalgo.usuario.model.Rol;
+
 import com.tfm.carnavalgo.usuario.repository.UsuarioRepository;
 import org.springframework.security.access.AccessDeniedException;
 
